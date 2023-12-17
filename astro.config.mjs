0dd8866199e574a24e5dd1d3901a4c3ed5438abb
Ivan Radev <107320114+R-zine@users.noreply.github.com>
2023-12-17 20:11:56 +0200

    Add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
-
 import svelte from "@astrojs/svelte";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [react(), vue(), svelte()],
+  adapter: netlify()
 });
