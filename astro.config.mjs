9d1b82f421497f9542383697e052c82937c52ec0
Ivan Radev <107320114+R-zine@users.noreply.github.com>
2023-12-18 11:54:42 +0200

    Fix htmx integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
 import svelte from "@astrojs/svelte";
+import htmx from "astro-htmx";
 
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [react(), vue(), svelte()],
-  adapter: netlify()
+  integrations: [htmx(), react(), vue(), svelte()],
+  adapter: netlify(),
 });
