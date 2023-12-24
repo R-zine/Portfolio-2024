7eb698b3347c4cc79c3001a19ff66737770e244f
Ivan Radev <107320114+R-zine@users.noreply.github.com>
2023-12-24 23:05:50 +0200

    Create Svelte page

---
+++ src/components/Svelte/Projects.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { onMount } from "svelte";
+
+    let message = "Svelte component"
+
+    onMount(()=> {
+        const spinner = document.querySelector(".spinner");
+        if (spinner) spinner.style.display = "none";})
+</script>
+
+<div>{message}</div>
